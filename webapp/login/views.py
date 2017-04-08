8e001387a5c9f7131d0da60ae81a2a041c272663
Thilina Abeysinghe <thilina.s.abeysinghe@gmail.com>
2017-04-07 23:41:59 -0700

    User DB created

--- webapp/login/views.py
+++ webapp/login/views.py
@@ -5,6 +5,8 @@ def login(request):
 	if request.method == "POST":
 			username = request.POST.get('username', '')
 			password = request.POST.get('password', '')
+
+			
 	else:
 		status = "Please try again "
 		return render(request, 'login.html', {'status':status})		
